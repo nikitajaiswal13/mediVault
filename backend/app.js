7e1a30478a900cf0d3607c1f77523fe389b50e09
Nikita Jaiswal <nikkijaiswal535@gmail.com>
2026-02-04 22:15:07 +0530

    Initial setup with server and app

---
+++ backend/app.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const dotenv = require('dotenv');
+
+dotenv.config({path : './.env'})
+const app = express();
+
+app.use(express.json());
+app.get('/' , (req , res) => {
+    res.send('MediVault Backend is running');
+})
+
+module.exports = app;
