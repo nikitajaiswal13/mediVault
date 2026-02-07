f2ca8b561ddb7ff93b9966e640e339665041b646
Nikita Jaiswal <nikkijaiswal535@gmail.com>
2026-02-07 13:52:07 +0530

    DB Connected

--- backend/app.js
+++ backend/app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const dotenv = require('dotenv');
 
 dotenv.config({path : './.env'})
+// console.log(dotenv.config({path : './.env'}));
 const app = express();
 
 app.use(express.json());
--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,17 @@
 const app = require('./app');
-
+const mongoose = require('mongoose');
 
 const port = process.env.PORT || 3000;
+
+mongoose.connect( process.env.DB , {
+    // useNewUrlParser: true,
+    // useUnifiedTopology: true
+}).then(() => {
+    console.log("Connection Done 😁")
+}).catch((err) => {
+    console.log("Connection Failed 💥" , err);
+})
+
 app.listen(port, () => {
     console.log(`App running on port ${port}...`);
 })
