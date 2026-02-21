bbe86473c86da1cc422201eb37176e336a42838d
Nikita Jaiswal <nikkijaiswal535@gmail.com>
2026-02-21 17:08:10 +0530

    Patient Function Done

--- frontend/src/app/pages/patients/patients.ts
+++ frontend/src/app/pages/patients/patients.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { HttpClient } from '@angular/common/http';
+import { Router } from '@angular/router';
+import { Patient } from '../../services/patient';
 
 @Component({
   selector: 'app-patients',
@@ -7,5 +11,43 @@ import { Component } from '@angular/core';
   styleUrl: './patients.css',
 })
 export class Patients {
+    patients: any[] = [];
+  patientForm: FormGroup;
+
+  constructor(
+    private patientService: Patient,
+    private fb: FormBuilder
+  ) {
+    this.patientForm = this.fb.group({
+      name: [''],
+      relation: [''],
+      dateOfBirth: ['']
+    });
+  }
+
+  ngOnInit() {
+    this.loadPatients();
+  }
+
+  loadPatients() {
+    this.patientService.getPatients().subscribe((res: any) => {
+      this.patients = res.data;
+    });
+  }
+
+  addPatient() {
+    this.patientService.createPatient(this.patientForm.value)
+      .subscribe(() => {
+        this.patientForm.reset();
+        this.loadPatients();
+      });
+  }
+
+  deletePatient(id: string) {
+    this.patientService.deletePatient(id)
+      .subscribe(() => {
+        this.loadPatients();
+      });
+  }
 
 }
---
+++ frontend/src/app/services/patient.ts
@@ -0,0 +1,31 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class Patient {
+  
+  private baseUrl = 'http://localhost:3000/api/v1/patients';
+
+  constructor(private http: HttpClient) {}
+
+  private getHeaders() {
+    const token = localStorage.getItem('token');
+    return new HttpHeaders({
+      Authorization: `Bearer ${token}`
+    });
+  }
+
+  getPatients() {
+    return this.http.get(this.baseUrl, { headers: this.getHeaders() });
+  }
+
+  createPatient(data: any) {
+    return this.http.post(this.baseUrl, data, { headers: this.getHeaders() });
+  }
+
+  deletePatient(id: string) {
+    return this.http.delete(`${this.baseUrl}/${id}`, { headers: this.getHeaders() });
+  }
+}
